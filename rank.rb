7b3f2475f937ccf52f3ac08deb11b6428fe0c3e2
Alexandria Cattron <acattron@gmail.com>
2014-07-03 15:01:53 -0700

    Implementing TDD
    Was able to get factorial method working correctly

--- rank.rb
+++ rank.rb
@@ -5,13 +5,29 @@ class RankGenerator
 		@computedFactorials = [1];
 	end
 
-	def rankWord(word)
+	def rank_word(word)
 		# If word is properly formatted...
 		# Proper format is a param that is a String with length between 2 and 25 inclusive.
-		# Characters must be in the alphabet and capitalized.  I am capitalizing all lower case letters.
-		if word.is_a?String && word.length >= @@MIN_WORD_SIZE && word.length <= @@MAX_WORD_SIZE && word.match(/^[A-Za-z]+$/)
+		# Characters must be in the alphabet and uppercase.  I am capitalizing all lowercase letters.
+		# There must be at least two different characters.
+		if word.is_a? String && word.length >= @@MIN_WORD_SIZE && word.length <= @@MAX_WORD_SIZE && word.match(/^[A-Za-z]+$/)
 			word.upcase!
-			
+			sorted_array_of_letters = word.split(//).sort
+			if (sorted_array_of_letters.uniq.length > 1) # only continue if at least two unique characters
+				word.l
+			end
+		end
+	end
+end
+
+class Integer < Numeric
+	def factorial
+		if self < 0
+			raise "Can't compute factorials of negative numbers."
+		elsif self > 0
+			return (1..self).inject(:*)
+		else
+			return 1
 		end
 	end
 end
---
+++ rank_spec.rb
@@ -0,0 +1,33 @@
+# Unit tests for rank.rb (rspec)
+require_relative "rank.rb"
+
+# describe Rank, "#rankWord" do
+# 	it "returns rank number for a correctly formatted word" do
+# 		rankMachine = Rank.new
+# 		r1 = rankMachine.rank_word "ABAB"
+# 		r2 = rankMachine.rank_word "AaAb"
+# 		r3 = rankMachine.rank_word "baaa"
+# 		r4 = rankMachine.rank_word "QUESTION"
+# 		r5 = rankMachine.rank_word "BOOKKEEPER"
+# 		r6 = rankMachine.rank_word "NONINTUITIVENESS"
+    	
+#     	r1.should.equal(2)
+#     	r2.should.equal(1)
+#     	r3.should.equal(4)
+#     	r4.should.equal(24572)
+#     	r5.should.equal(10743)
+#     	r6.should.equal(8222334634)
+#   	end
+# end
+
+describe Integer, "#factorial" do
+	it "returns the factorial of a number" do
+		0.factorial.should eq(1)
+		1.factorial.should eq(1)
+		6.factorial.should eq(720)
+
+		expect { -1.factorial }.to raise_error("Can't compute factorials of negative numbers.")
+	end
+end
+
+		
